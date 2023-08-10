b1fcdf5e23c2a4445b4770518782634eb1d0d918
Jakub Musialowski <mds@mds.biz.pl>
2023-08-10 23:07:24 +0200

    Install styled components, add global style component

--- src/App.js
+++ src/App.js
@@ -6,37 +6,42 @@ import Tasks from './Tasks';
 import TaskMenu from './TasksMenu';
 import Section from './Section';
 import { useTasks } from "./useTasks";
+import StyledGlobal from './Styled/styledGlobal';
 
 function App() {
 
   const [tasksList, addNewTask, toggleTaskDone, doneAllTasks, hideDoneTasks, deleteTask] = useTasks()
 
   return (
-    <TaskList>
-      <Header title="Lista zadań" />
-      <Section 
-        title="Dodaj nowe zadanie" 
-        body={
-          <Form 
-            addNewTask={addNewTask}
-          />} 
-      />
-      <Section 
-        title="Lista zadań" 
-        menu={
-          <TaskMenu 
-            tasks={tasksList} 
-            doneAllTasks={doneAllTasks}
-            hideDoneTasks={hideDoneTasks}
-          />} 
-        body={
-          <Tasks 
-            tasks={tasksList} 
-            toggleTaskDone={toggleTaskDone}
-            deleteTask={deleteTask}
-          />} 
-      />
-    </TaskList>
+    <>
+      <StyledGlobal />
+      <TaskList>
+        <Header title="Lista zadań" />
+        <Section
+          title="Dodaj nowe zadanie"
+          body={
+            <Form
+              addNewTask={addNewTask}
+            />}
+        />
+        <Section
+          title="Lista zadań"
+          menu={
+            <TaskMenu
+              tasks={tasksList}
+              doneAllTasks={doneAllTasks}
+              hideDoneTasks={hideDoneTasks}
+            />}
+          body={
+            <Tasks
+              tasks={tasksList}
+              toggleTaskDone={toggleTaskDone}
+              deleteTask={deleteTask}
+            />}
+        />
+      </TaskList>
+    </>
+
   );
 }
 
