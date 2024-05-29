d5327b371c7652c6de7cfa452d66312b4bf46f75
Jakub Musialowski <mds@mds.biz.pl>
2024-05-30 00:58:10 +0200

    Remove unnecessary code

--- src/features/tasks/TasksMenu/index.js
+++ src/features/tasks/TasksMenu/index.js
@@ -14,10 +14,6 @@ const TaskMenu = () => {
             <StyledMenuItem 
                 onClick={() => dispatch(hideDoneTasks())}
             >
-                {/* {tasks.some(({ taskVisibility }) => taskVisibility === false) 
-                ? "Pokaż " 
-                : "Ukryj "} */}
-
                 {isHiddenTasks
                 ? "Pokaż " 
                 : "Ukryj "}
