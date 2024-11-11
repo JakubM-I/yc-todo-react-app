e03c15548b1f04428985cfcaca606177ad76791d
Jakub Musialowski <mds@mds.biz.pl>
2024-11-11 21:00:07 +0100

    Add content to author page

--- src/features/author/index.js
+++ src/features/author/index.js
@@ -9,10 +9,12 @@ const AuthorPage = () => (
         <PageTitle title={"Lista zadań - O autorze"} />
         <TasksAppMain>
             <Header title={"O autorze"} />
-            <Section body={
-                <StyledAuthorPageBody>
-                    Tekst o autorze
-                </StyledAuthorPageBody>}
+            <Section
+                title={"Jakub Musiałowski"}
+                body={
+                    <StyledAuthorPageBody>
+                        <p>Od lat jestem miłośnikiem internetu i wszystkiego co z nim związane. <strong>Od przeszło 15 lat</strong> jestem zwiazany z branżą e-commerce tworząc, rozwijając i prowadząc sklepy internetowe. W pracy tej zawsze gdzieś ocierałem się o pracę z kodem, wprowadzanie zmian w sklepach czy dodawanie nowych elementów. Dlatego od ponad roku intensywnie zgłębiam tajniki frontdeveloperki. I okazało się..., że stało się to moją <strong>ogromną pasją.</strong> </p>
+                    </StyledAuthorPageBody>}
             />
         </TasksAppMain>
 
